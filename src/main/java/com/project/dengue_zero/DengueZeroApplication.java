1c8082cd07650ec4e0c5c5883994f528417033e2
devartur <arturvinicius.estudante.trabalho@gmail.com>
2025-09-20 14:57:04 -0300

    Criando projeto

---
+++ src/main/java/com/project/dengue_zero/DengueZeroApplication.java
@@ -0,0 +1,13 @@
+package com.project.dengue_zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DengueZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DengueZeroApplication.class, args);
+	}
+
+}
